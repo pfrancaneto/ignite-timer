169ff53359046a6ebdd7692cb18317f2d17a1d1e
Pedro França <pfranca.neto@gmail.com>
2025-05-02 22:45:18 -0300

    ajustes nos campos inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,10 +19,24 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list='list-suggestions'
           />
 
+          <datalist id='list-suggestions'>
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="text" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
