aeced3af25365df24128f757a951399b9fb3cda7
Pedro França <pfranca.neto@gmail.com>
2025-05-04 21:26:54 -0300

    ajustes componente Historico

--- src/pages/Historico/index.tsx
+++ src/pages/Historico/index.tsx
@@ -1,5 +1,48 @@
+import { HistoricoContainer, HistoricoLista } from './styles';
+
 export function Historico() {
   return (
-    <h1>Historico</h1>
-  )
+    <HistoricoContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoricoLista>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoricoLista>
+    </HistoricoContainer>
+  );
 }
